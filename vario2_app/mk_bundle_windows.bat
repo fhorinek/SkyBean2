6590be6451d7cd336e0d4a90fc2a68327c40a688
František Horínek <frantisekhorinek@gmail.com>
2018-10-02 15:02:00 +0200

    PC app update
    Linux binary, windows bundel bat

---
+++ vario2_app/mk_bundle_windows.bat
@@ -0,0 +1,8 @@
+
+REM remove chache
+rm -rf build
+REM remove binary
+rm dist/*_windows
+
+pyinstaller -w --onefile --add-data "data;data" main.py --workpath "build" --name "skybean2_app_windows" --icon "data/icon.ico"
+

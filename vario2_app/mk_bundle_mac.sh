bf7b0336f150aebfefcfe3256c8a072e18680d67
Frantisek Horinek <horinek@macmini.local>
2018-10-29 12:32:20 +0100

    added dmg for mac

--- vario2_app/mk_bundle_mac.sh
+++ vario2_app/mk_bundle_mac.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-echo "type pyenv activate mac_env first"
+echo "first type 'pyenv activate mac_env'"
 
 #clear chache
 rm -rf build
 #remove binary
 rm -rf dist/*_mac*
 
-#pyinstaller -w --onefile --add-data "data:data" main.py --workpath "build" --name "skybean2_app_mac" --icon "data/icon.icns"
 pyinstaller -w --add-data "data:data" main.py --workpath "build" --name "skybean2_app_mac" --icon "data/icon.icns"
 
-zip -r dist/skybean2_app_mac.zip dist/skybean2_app_mac.app
-rm -rf dist/skybean2_app_mac.app
+mv dist/skybean2_app_mac.app dist/SkyBean2.app
+dmgbuild -s skybean2_app_mac.image a a
 rm -rf dist/skybean2_app_mac
+rm -rf dist/SkyBean2.app
+
 
 
 
--- vario2_prog/mk_bundle_mac.sh
+++ vario2_prog/mk_bundle_mac.sh
@@ -9,3 +9,7 @@ cp ../vario2/Release/vario2.hex bundled.hex
 
 source mac_env/bin/activate
 pyinstaller --onefile --add-data "bundled.hex:." prog.py --workpath "build" --name "skybean2_update_mac" --icon=icon.icns
+
+mv dist/skybean2_update_mac "dist/SkyBean2 firmware update"
+dmgbuild -s skybean2_update_mac.image a a
+rm -rf "dist/SkyBean2 firmware update"
